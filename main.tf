c3fac0150a1d9eb66d00612670ad1dddea2cf80e
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2024-05-31 17:52:21 -0500

    ran terraform fmt.

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
@@ -20,8 +20,8 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
-  vpc_cidr = var.vpc_cidr
-  enable_ipv6 = var.enable_ipv6
+  source       = "./modules/vpc"
+  vpc_cidr     = var.vpc_cidr
+  enable_ipv6  = var.enable_ipv6
   vpc_tag_name = var.vpc_tag_name
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,3 @@
-vpc_cidr = "192.168.30.0/24"
-enable_ipv6 = true
+vpc_cidr     = "192.168.30.0/24"
+enable_ipv6  = true
 vpc_tag_name = "Rocket-Bank-PROD"
