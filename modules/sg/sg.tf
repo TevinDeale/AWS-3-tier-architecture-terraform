7f479cd471263e21cda5f257c6bb6e8f0a98828a
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2024-06-05 23:09:48 -0500

    setup ingress rule resource in sg.tf

--- modules/sg/sg.tf
+++ modules/sg/sg.tf
@@ -29,8 +29,39 @@ locals {
     description = rule.description
   } } : {}
 
+  ingress_rules = var.sg_egress_rule != null ? { for rule in var.sg_ingress_rule : rule.name => {
+    name        = rule.name
+    sg_id       = rule.sg_id
+    ip_proto    = rule.ip_proto
+    from_port   = rule.from_port
+    to_port     = rule.to_port
+    cidr_ipv4   = rule.source_ipv4
+    cidr_ipv6   = rule.source_ipv6
+    ref_sg_id   = rule.source_sg_id
+    description = rule.description
+  } } : {}
+
   sg_name_to_id = { for sg in aws_security_group.main_sg : sg.name => sg.id }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "sg_ingress_rule" {
+  for_each = local.ingress_rules
+
+  security_group_id = lookup(local.sg_name_to_id, each.value.sg_id)
+
+  cidr_ipv4                    = each.value.cidr_ipv4
+  cidr_ipv6                    = each.value.cidr_ipv6
+  referenced_security_group_id = each.value.ref_sg_id != null ? lookup(local.sg_name_to_id, each.value.ref_sg_id) : null
+  ip_protocol                  = each.value.ip_proto
+  from_port                    = each.value.from_port
+  to_port                      = each.value.to_port
+  description                  = each.value.description
+
+  tags = {
+    Name = each.value.name
+  }
+}
+
 resource "aws_vpc_security_group_egress_rule" "sg_egress_rule" {
   for_each = local.egress_rules
 
