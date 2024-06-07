77281f5f25f10f9375f7c5bf56a95991370baaa4
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2024-06-07 15:34:00 -0500

    added all ec2 instances with outputs

---
+++ modules/ec2/outputs.tf
@@ -0,0 +1,4 @@
+output "instances" {
+  description = "EC2 INSTANCES DETAILS"
+  value       = aws_instance.ec2
+}
--- outputs.tf
+++ outputs.tf
@@ -27,3 +27,25 @@ output "security_groups" {
   description = "SECURITY GROUP DETAILS"
   value       = module.sg.security_groups
 }
+
+output "instances" {
+  description = "EC2 INSTANCES DETAILS"
+  value = [
+    for instance in module.instance.instances : {
+      name         = instance.tags["Name"],
+      arn          = instance.arn
+      id           = instance.id
+      ipv6_address = instance.ipv6_addresses
+      ipv4_private = instance.private_ip
+      ipv4_public  = instance.public_ip
+      az           = instance.availability_zone
+      subnet       = lookup({ for sn in module.vpc.subnets : sn.ID => sn.name }, instance.subnet_id)
+      sg           = instance.security_groups
+    }
+  ]
+}
+
+output "instance_private_ips" {
+  description = "EC2 INSTANCE PRIVATE IPV4 ADDRESSES"
+  value       = [for instance in module.instance.instances : instance.private_ip]
+}
